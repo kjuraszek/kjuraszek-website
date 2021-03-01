c7d1dd465058f5923f6775cecdae7be7818ab4fc
kjuraszek <donkrissocorleone@gmail.com>
2021-03-01 16:18:37 +0100

    Using axios instead import
    Using axios instead import to gather projects data.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,6 +18,7 @@ import WhatsNew from './WhatsNew.vue'
 import Portfolio from './Portfolio.vue'
 import Contact from './Contact.vue'
 import Footer from './Footer.vue'
+import axios from 'axios';
 
 export default {
   name: 'Home',
@@ -45,13 +46,16 @@ export default {
   methods: {
       getProjectsData() {
           this.loading = true
-          import("@/data/projects.json")
-          .then(({default: json}) => {
+          axios.get(process.env.BASE_URL+"data/projects.json")
+          .then((response) => {
             this.loading = false;
-            this.projects = json.projects.map( (project) => {
+            this.projects = response.data.projects.map( (project) => {
               project.image = project.image ? require('@/assets/' + project.image) : this.defaultImage;
               return project;
           })
+          })
+          .catch(error => {
+            console.log(error);
           })
       }
   }
