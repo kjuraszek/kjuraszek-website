5da29a8625882a2753677ee85d0420e8cc0927df
kjuraszek <donkrissocorleone@gmail.com>
2021-03-03 07:37:18 +0100

    Update Portfolio.vue
    Filtering projects by tags

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -3,15 +3,35 @@
     <div class="container">
       <h2 class="title is-2">Chapter 3: Projects</h2>
       <p class="subtitle"><em>dragons, epic battles, powerful spells ? forget it, this is far more thrilling!</em></p>
-      <p class="is-6" v-if="!loading"><em>showing {{ items.length.toString() + (items.length === 1 ? " project" : " projects")}} from newest</em></p>
       <template v-if="!loading">
-      <div class="columns is-desktop is-multiline">
-        <PortfolioItem 
-        v-for="(item, index) in items" 
-        v-bind:item="item"
-        v-bind:index="index"
-        :key="item.id" />
-      </div>
+        <div class="buttons">
+          <b-button 
+          v-for="(tag, index) in tags"
+          @click="changeTag($event, tag)"
+          :key="index" 
+          :type="tag===filteredTag ? 'is-danger':'is-danger is-light'"
+          >
+            {{tag}}
+          </b-button>
+        </div>
+        <p class="is-6" v-if="!loading">
+          <em>showing {{ items.length.toString() + (items.length === 1 ? " project" : " projects")}} from newest</em>
+        </p>
+        
+        <div >
+          <transition-group
+          tag="div" 
+          name="fade" 
+          mode="out-in"
+          class="columns is-desktop is-multiline">
+            <PortfolioItem 
+          v-for="(item, index) in items" 
+          v-bind:item="item"
+          v-bind:index="index"
+          :key="item.id" />
+          </transition-group>
+        </div>
+        
       </template>
       <b-skeleton size="is-large" :count="7" :active="loading"></b-skeleton>
     </div>
@@ -23,16 +43,45 @@ import PortfolioItem from './PortfolioItem.vue'
 
 export default {
   name: 'Portfolio',
+  data(){
+    return{
+      selectedTag: 'all'
+    }
+  },
+  methods:{
+    changeTag: function(event, t){
+      this.selectedTag = t
+    }
+  },
   computed:{
+    filteredTag: function(){
+      return this.selectedTag;
+    },
     loading: function(){
-      return this.$parent.loading
+      return this.$parent.loading;
     },
     items: function(){
-      return this.$parent.projects.length > 0 ? [...this.$parent.projects].sort((a,b) => b.id - a.id) : []
+      if(this.$parent.projects.length === 0){
+        return [];
+      } else {
+        if( this.selectedTag === "all"){
+          return  [...this.$parent.projects].sort((a,b) => b.id - a.id);
+        } else {
+          return  [...this.$parent.projects].sort((a,b) => b.id - a.id)
+          .filter( project => project.tags.includes(this.selectedTag));
+        }
+      }
+    },
+    tags: function(){
+      let projects = this.$parent.projects;
+      return projects.length > 0 ? 
+      [... new Set(["all"].concat(...projects.map( (project) => project.tags)))]
+      .sort((a,b) => a > b) : 
+      []
     }
   },
   components:{
     PortfolioItem
-  }
+  },
 }
 </script>
